b3aa0bba8f85b9c4e82ca20e8608121f3ae9aaff
Yuriy Nalivayko <yura.nlvko@gmail.com>
2019-01-26 21:09:38 +0200

    Add GoBack button

--- app/src/main/java/com/agileengine/ynlvko_test/MainActivity.kt
+++ app/src/main/java/com/agileengine/ynlvko_test/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.agileengine.ynlvko_test
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.agileengine.ynlvko_test.images.image_details.ImageDetailsFragment
 import com.agileengine.ynlvko_test.images.image_list.ImageListFragment
 
 class MainActivity : AppCompatActivity(), Navigator {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,18 +16,28 @@ class MainActivity : AppCompatActivity(), Navigator {
         }
     }
 
-    private fun setupImageListFragment() {
-        val fragmentManager = supportFragmentManager
-        fragmentManager.beginTransaction()
-            .add(R.id.fragmentContainer, ImageListFragment.newInstance())
-            .commit()
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            supportFragmentManager.popBackStack()
+            supportActionBar?.setDisplayHomeAsUpEnabled(false)
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun showImageDetails(imagePosition: Int) {
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
         val fragmentManager = supportFragmentManager
         fragmentManager.beginTransaction()
             .addToBackStack(null)
             .add(R.id.fragmentContainer, ImageDetailsFragment.newInstance(imagePosition))
+            .commit()
+    }
+
+    private fun setupImageListFragment() {
+        val fragmentManager = supportFragmentManager
+        fragmentManager.beginTransaction()
+            .add(R.id.fragmentContainer, ImageListFragment.newInstance())
             .commit()
     }
 }
