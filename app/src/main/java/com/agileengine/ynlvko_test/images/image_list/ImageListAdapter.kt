52ced219434be6ac414265a1fdfd26f32c018661
Yuriy Nalivayko <yura.nlvko@gmail.com>
2019-01-26 20:37:27 +0200

    Load fake images with Picasso into SquredImageView

--- app/src/main/java/com/agileengine/ynlvko_test/images/image_list/ImageListAdapter.kt
+++ app/src/main/java/com/agileengine/ynlvko_test/images/image_list/ImageListAdapter.kt
@@ -1,22 +1,27 @@
 package com.agileengine.ynlvko_test.images.image_list
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
+import com.agileengine.ynlvko_test.R
 import com.agileengine.ynlvko_test.images.Image
+import com.squareup.picasso.Picasso
 
 class ImageListAdapter : RecyclerView.Adapter<ImageItemVH>() {
     private var imageList = listOf<Image>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageItemVH {
-        return ImageItemVH(TextView(parent.context))
+        val itemView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_view_image, parent, false)
+        return ImageItemVH(itemView)
     }
 
     override fun getItemCount() = imageList.size
 
     override fun onBindViewHolder(holder: ImageItemVH, position: Int) {
-
+        holder.bind(imageList[position])
     }
 
     fun updateImages(images: List<Image>) {
@@ -26,5 +31,9 @@ class ImageListAdapter : RecyclerView.Adapter<ImageItemVH>() {
 }
 
 class ImageItemVH(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    private val imageView = itemView.findViewById<ImageView>(R.id.ivImage)
 
+    fun bind(image: Image) {
+        Picasso.get().load(image.url).into(imageView)
+    }
 }
---
+++ app/src/main/java/com/agileengine/ynlvko_test/views/SquareImageView.kt
@@ -0,0 +1,18 @@
+package com.agileengine.ynlvko_test.views
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatImageView
+
+
+class SquareImageView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : AppCompatImageView(context, attrs, defStyleAttr) {
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+
+        val width = measuredWidth
+        setMeasuredDimension(width, width)
+    }
+}
